810e7347078ed57e04d9244e07ee5c354a9e017e
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-06-29 12:32:49 +0200

    add routes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+export default function ProjectsPage() {
+  return (
+    <h1 className="font-serif text-3xl font-bold tracking-tight">About Page</h1>
+  );
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,7 @@
+export default function ProjectsPage() {
+  return (
+    <h1 className="font-serif text-3xl font-bold tracking-tight">
+      Projects Page
+    </h1>
+  );
+}
